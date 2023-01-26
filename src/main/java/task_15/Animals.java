9ff6a4fa37feb0ae870df47303d38ac1cd180959
Bohdan Raman <bohdanraman@gmail.com>
2023-01-26 23:38:29 +0300

    add description in task 15

--- src/main/java/task_15/Animals.java
+++ src/main/java/task_15/Animals.java
@@ -1,5 +1,15 @@
 package task_15;
 
+/**
+ * Exercise 14: Create three interfaces, each with two methods. Inherit a new interface that combines the three, adding a new method.
+ * Create a class by implementing the new interface and also inheriting from a concrete class.
+ * Now write four methods, each of which takes one of the four interfaces as an argument.
+ * In main( ), create an object of your class and pass it to each of the methods
+ * <p>
+ * Exercise 15: Modify the previous exercise by creating an abstract class and
+ * inheriting that into the derived class.
+ */
+
 public class Animals extends AnimalsCharacteristic implements AgeAnimals {
     @Override
     public void ageAnimals() {
@@ -51,7 +61,7 @@ public class Animals extends AnimalsCharacteristic implements AgeAnimals {
         eat.eatVegetables();
     }
 
-    public void fourthMethod(AgeAnimals age){
+    public void fourthMethod(AgeAnimals age) {
         age.ageAnimals();
     }
 }
